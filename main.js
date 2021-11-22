563f1192f3fc6f229c34d91cc859d353a49cb552
David Thompson <matkatdavid@gmail.com>
2021-11-22 16:48:56 -0700

    Made dropdown clickable

---
+++ main.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector("#dropdown");
+const listItem = document.querySelector("#dropdown ul li");
+const buttons = document.querySelector(".button");
+
+menu.addEventListener("touchend", function (e) {
+  e.preventDefault();
+  const menuItem = document.querySelector("#dropdown > li ul");
+  menuItem.classList.toggle("open");
+});
+
+menu.addEventListener("click", function () {
+  const menuItem = document.querySelector("#dropdown > li ul");
+  menuItem.classList.toggle("open");
+});
+
+listItem.addEventListener("click", function () {
+  listItem.classList.toggle("clicked");
+});
+
+// buttons.addEventListener("click", function () {
+//   buttons.firstChild.
+// });
